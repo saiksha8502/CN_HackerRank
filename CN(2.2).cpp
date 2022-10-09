35d6ff7e0ed03d322f72e989d3ce2e4d9401acdf
Saiksha Desai <sd0040@srmist.edu.in>
2022-10-09 11:20:26 +0530

    Create CN(2.2).cpp

---
+++ CN(2.2).cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+cout<<"Server output:"<<endl<<"Sent:  message #1"<<endl<<"Sent:  message #2"<<endl<<"Sent:  message #3"<<endl<<"Sent:  message #4"<<endl<<endl<<"Client Output:"<<endl<<"Received following data on stream 0"<<endl<<"Message #1"<<endl<<"Received following data on stream 1"<<endl<<"Message #2"<<endl<<"Received following data on stream 2"<<endl<<"Message #3"<<endl<<"Received following data on stream 3"<<endl<<"Message #4";
+         
+    return 0;
+}
